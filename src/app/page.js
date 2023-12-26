ab154f8b9c8d51853229a466082560f052e13f7d
FazlayRabbi <rabbisam74@gmail.com>
2023-12-26 18:17:46 +0600

    add lucide

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
     <>
-      <Button> hello</Button>
+      <Navbar />
     </>
   );
 }
